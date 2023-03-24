8894154c01f810711523416f3ddeec058ffe4364
ffekirnew <ffekirnew0808@gmail.com>
2023-03-24 23:34:19 +0300

    added some new changes

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
 
 
 class Post(models.Model):
@@ -19,8 +24,20 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                        self.publish.month,
+                        self.publish.day,
+                        self.slug]
+        )
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    return render(request,
+        'blog/post/detail.html',
+        {'post': post})
