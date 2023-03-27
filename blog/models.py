a403bedde4c3bdc7a800d03f8b007dbaea0c98ce
ffekirnew <ffekirnew0808@gmail.com>
2023-03-27 13:48:40 +0300

    added some changes

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset()\
-                                            .filter(status='published')
+        return super(PublishedManager, self).get_queryset() \
+            .filter(status='published')
 
 
 class Post(models.Model):
@@ -23,6 +24,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
     objects = models.Manager()
     published = PublishedManager()
@@ -35,9 +37,24 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                        args=[self.publish.year,
-                        self.publish.month,
-                        self.publish.day,
-                        self.slug]
-        )
-    
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug]
+                       )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on the post {self.post}."
+
+    class Meta:
+        ordering = ('created',)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
+
+from .forms import EmailPostForm, CommentPostForm
 from .models import Post
 from django.views.generic import ListView
 
@@ -10,12 +14,85 @@ class PostListView(ListView):
     paginate_by = 2
     template_name = 'blog/post/list.html'
 
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts,
+                   'tag': tag})
+
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                                    status='published',
-                                    publish__year=year,
-                                    publish__month=month,
-                                    publish__day=day)
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+    add_new_comment_form = None
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentPostForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        add_new_comment_form = CommentPostForm()
+
     return render(request,
-        'blog/post/detail.html',
-        {'post': post})
+                  'blog/post/detail.html',
+                  {
+                      'post': post,
+                      'comments': comments,
+                      'comment_form': add_new_comment_form,
+                      'new_comment': new_comment
+                  })
+
+
+def post_share(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'ffekirnew0808@gmail.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
